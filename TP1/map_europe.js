fb7d2789aae98e4a965ca4195284b0474ff65c88
Roubia Akram <114688151+Muugen0@users.noreply.github.com>
2025-09-26 16:01:29 +0200

    Correction du script map_europe.js
    Fait en sorte que les informations communiquées ne concernet que les personnes de 15-24 ans

--- TP1/map_europe.js
+++ TP1/map_europe.js
@@ -1,12 +1,12 @@
 const map = eurostatmap
                 .map('choropleth')
-                .title('Self-reported consultation of mental healthcare')
+                .title('Self-reported consultation of mental healthcare for 15-24 year olds')
                 .width(1080)
                 .subtitle('2014')
-                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2014', sex: 'T', isco08: 'OC2634_2212PSY'  }})
+                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2014', sex: 'T', isco08: 'OC2634_2212PSY', age: 'Y15-24'  }})
                 .legend({ x: 10, y: 200, title: 'Percentage of consultations, %', boxOpacity: 0 })
-                .numberOfClasses(6)
-                .colors(['#E97452', '#D65D42', '#C34632', '#B12E21', '#9E1711', '#8B0001'])
+                .numberOfClasses(5)
+                .colors(['#E97452', '#D65D42',  '#B12E21', '#9E1711', '#8B0001'])
                 .nutsLevel(0)
                 .build()
 
@@ -17,13 +17,13 @@ const map = eurostatmap
 document.getElementById("2014_button").addEventListener("click", function() {
     const map = eurostatmap
                 .map('choropleth')
-                .title('Self-reported consultation of mental healthcare')
+                .title('Self-reported consultation of mental healthcare for 15-24 year olds')
                 .width(1080)
                 .subtitle('2014')
-                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2014', sex: 'T', isco08: 'OC2634_2212PSY'  }})
+                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2014', sex: 'T', isco08: 'OC2634_2212PSY', age: 'Y15-24'  }})
                 .legend({ x: 10, y: 200, title: 'Percentage of consultations, %', boxOpacity: 0 })
-                .numberOfClasses(6)
-                .colors(['#E97452', '#D65D42', '#C34632', '#B12E21', '#9E1711', '#8B0001'])
+                .numberOfClasses(5)
+                .colors(['#E97452', '#D65D42', '#B12E21', '#9E1711', '#8B0001'])
                 .nutsLevel(0)
                 .build()
 });
@@ -31,13 +31,13 @@ document.getElementById("2014_button").addEventListener("click", function() {
 document.getElementById("2019_button").addEventListener("click", function() {
     const map = eurostatmap
                 .map('choropleth')
-                .title('Self-reported consultation of mental healthcare')
+                .title('Self-reported consultation of mental healthcare for 15-24 year olds')
                 .width(1080)
                 .subtitle('2019')
-                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2019', sex: 'T', isco08: 'OC2634_2212PSY'  }})
+                .stat({eurostatDatasetCode: 'hlth_ehis_am6e', unitText: '%', filters:{TIME: '2019', sex: 'T', isco08: 'OC2634_2212PSY', age: 'Y15-24'  }})
                 .legend({ x: 10, y: 200, title: 'Percentage of consultations, %', boxOpacity: 0 })
-                .numberOfClasses(6)
-                .colors(['#E97452', '#D65D42', '#C34632', '#B12E21', '#9E1711', '#8B0001'])
+                .numberOfClasses(5)
+                .colors(['#E97452', '#D65D42',  '#B12E21', '#9E1711', '#8B0001'])
                 .nutsLevel(0)
                 .build()
 });

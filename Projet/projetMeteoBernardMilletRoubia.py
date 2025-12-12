4fd2a0e0ee0f2ee9c2676713aace122442bdf485
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2025-12-12 10:16:53 +0100

    base projet

--- Projet/projetMeteoBernardMilletRoubia.py
+++ Projet/projetMeteoBernardMilletRoubia.py
@@ -20,12 +20,11 @@ renderView1 = CreateView('RenderView')
 renderView1.Set(
     ViewSize=[1418, 779],
     InteractionMode='2D',
-    CenterOfRotation=[-2.9865554327534554, 17.00513539215395, 0.0],
-    UseLight=0,
-    CameraPosition=[4.482935863875653, 46.80327201197917, 271.4051587004233],
-    CameraFocalPoint=[4.482935863875653, 46.80327201197917, 0.0],
+    CenterOfRotation=[2.0, 46.45000076293945, 0.0],
+    CameraPosition=[3.039810611739514, 46.37270127095975, 64.20057971900172],
+    CameraFocalPoint=[3.039810611739514, 46.37270127095975, 0.0],
     CameraFocalDisk=1.0,
-    CameraParallelScale=10.441445490874104,
+    CameraParallelScale=11.349178838809015,
     OSPRayMaterialLibrary=materialLibrary1,
 )
 
@@ -50,38 +49,32 @@ SetActiveView(renderView1)
 # ----------------------------------------------------------------
 
 # create a new 'NetCDF Reader'
-a12dec6hnc = NetCDFReader(registrationName='12dec6h.nc', FileName=['/home/pierre/Documents/Info5/Visu/Projet/ParaviewData/12dec/12dec6h.nc'])
+a12dec6hnc = NetCDFReader(registrationName='12dec6h.nc', FileName=['/home/pierre/Documents/Info5/Visu/VISU_DONNEES_INFO5/Projet/ParaviewData/12dec/12dec6h.nc'])
 a12dec6hnc.Set(
     Dimensions='(latitude, longitude)',
     SphericalCoordinates=0,
 )
 
 # create a new 'GDAL Vector Reader'
-departements20140306100mshp = GDALVectorReader(registrationName='departements-20140306-100m.shp', FileName='/home/pierre/Documents/Info5/Visu/Projet/departements-20140306-100m-shp/departements-20140306-100m.shp')
+departements20140306100mshp = GDALVectorReader(registrationName='departements-20140306-100m.shp', FileName='/home/pierre/Documents/Info5/Visu/VISU_DONNEES_INFO5/Projet/departements-20140306-100m-shp/departements-20140306-100m.shp')
+
+# create a new 'Calculator'
+calculator1 = Calculator(registrationName='Calculator1', Input=a12dec6hnc)
+calculator1.Set(
+    ResultArrayName='Temperature',
+    Function='t2m - 273.15',
+)
+
+# create a new 'Calculator'
+calculator2 = Calculator(registrationName='Calculator2', Input=calculator1)
+calculator2.Set(
+    ResultArrayName='VentVecteur',
+    Function='u10*iHat+v10*jHat',
+)
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
 # ----------------------------------------------------------------
-
-# show data from a12dec6hnc
-a12dec6hncDisplay = Show(a12dec6hnc, renderView1, 'StructuredGridRepresentation')
-
-# get color transfer function/color map for 't2m'
-t2mLUT = GetColorTransferFunction('t2m')
-t2mLUT.Set(
-    RGBPoints=GenerateRGBPoints(
-        range_min=256.45910092278245,
-        range_max=290.7166854990885,
-    ),
-    ScalarRangeInitialized=1.0,
-)
-
-# trace defaults for the display properties.
-a12dec6hncDisplay.Set(
-    Representation='Surface',
-    ColorArrayName=['POINTS', 't2m'],
-    LookupTable=t2mLUT,
-)
 
 # show data from departements20140306100mshp
 departements20140306100mshpDisplay = Show(departements20140306100mshp, renderView1, 'GeometryRepresentation')
@@ -93,31 +86,56 @@ departements20140306100mshpDisplay.Set(
     Assembly='Hierarchy',
 )
 
+# show data from calculator2
+calculator2Display = Show(calculator2, renderView1, 'RectilinearGridRepresentation')
+
+# get color transfer function/color map for 'VentVecteur'
+ventVecteurLUT = GetColorTransferFunction('VentVecteur')
+ventVecteurLUT.Set(
+    RGBPoints=GenerateRGBPoints(
+        range_min=0.006810075996320468,
+        range_max=20.409033997143062,
+    ),
+    ScalarRangeInitialized=1.0,
+)
+
+# trace defaults for the display properties.
+calculator2Display.Set(
+    Representation='Surface',
+    ColorArrayName=['POINTS', 'VentVecteur'],
+    LookupTable=ventVecteurLUT,
+)
+
+# init the 'Piecewise Function' selected for 'ScaleTransferFunction'
+calculator2Display.ScaleTransferFunction.Points = [-16.690899077217523, 0.0, 0.5, 0.0, 17.56668549908852, 1.0, 0.5, 0.0]
+
+# init the 'Piecewise Function' selected for 'OpacityTransferFunction'
+calculator2Display.OpacityTransferFunction.Points = [-16.690899077217523, 0.0, 0.5, 0.0, 17.56668549908852, 1.0, 0.5, 0.0]
+
 # setup the color legend parameters for each legend in this view
 
-# get color legend/bar for t2mLUT in view renderView1
-t2mLUTColorBar = GetScalarBar(t2mLUT, renderView1)
-t2mLUTColorBar.Set(
-    WindowLocation='Upper Right Corner',
-    Title='t2m',
-    ComponentTitle='',
+# get color legend/bar for ventVecteurLUT in view renderView1
+ventVecteurLUTColorBar = GetScalarBar(ventVecteurLUT, renderView1)
+ventVecteurLUTColorBar.Set(
+    Title='VentVecteur',
+    ComponentTitle='Magnitude',
 )
 
 # set color bar visibility
-t2mLUTColorBar.Visibility = 1
+ventVecteurLUTColorBar.Visibility = 1
 
 # show color legend
-a12dec6hncDisplay.SetScalarBarVisibility(renderView1, True)
+calculator2Display.SetScalarBarVisibility(renderView1, True)
 
 # ----------------------------------------------------------------
 # setup color maps and opacity maps used in the visualization
 # note: the Get..() functions create a new object, if needed
 # ----------------------------------------------------------------
 
-# get opacity transfer function/opacity map for 't2m'
-t2mPWF = GetOpacityTransferFunction('t2m')
-t2mPWF.Set(
-    Points=[256.45910092278245, 0.0, 0.5, 0.0, 290.7166854990885, 1.0, 0.5, 0.0],
+# get opacity transfer function/opacity map for 'VentVecteur'
+ventVecteurPWF = GetOpacityTransferFunction('VentVecteur')
+ventVecteurPWF.Set(
+    Points=[0.006810075996320468, 0.0, 0.5, 0.0, 20.409033997143062, 1.0, 0.5, 0.0],
     ScalarRangeInitialized=1,
 )
 
@@ -153,7 +171,7 @@ animationScene1.Set(
 
 # ----------------------------------------------------------------
 # restore active source
-SetActiveSource(a12dec6hnc)
+SetActiveSource(calculator2)
 # ----------------------------------------------------------------
 
 

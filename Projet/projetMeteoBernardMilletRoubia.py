018699c4c1b6224b9321ea44aeeb18dbd335f423
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2025-12-12 13:52:23 +0100

    dual vision

--- Projet/projetMeteoBernardMilletRoubia.py
+++ Projet/projetMeteoBernardMilletRoubia.py
@@ -1,6 +1,7 @@
 # state file generated using paraview version 6.0.1
 import paraview
 import sys
+import math
 paraview.compatibility.major = 6
 paraview.compatibility.minor = 0
 
@@ -20,36 +21,55 @@ paraview.simple._DisableFirstRenderCameraReset()
 # setup views used in the visualization
 # ----------------------------------------------------------------
 
+fileName = sys.argv[1]
+longitude = float(sys.argv[2])
+latitude = float(sys.argv[3])
 
 # get the material library
 materialLibrary1 = GetMaterialLibrary()
 
-# Create a new 'Render View'
+# Create the first 'Render View' (for the first window)
 renderView1 = CreateView('RenderView')
 renderView1.Set(
-    ViewSize=[1418, 779],
+    ViewSize=[961, 479],
     InteractionMode='2D',
-    CenterOfRotation=[2.0, 46.441402435302734, 0.0],
-    CameraPosition=[2.0, 46.441402435302734, 63.87260524857851],
-    CameraFocalPoint=[2.0, 46.441402435302734, 0.0],
+    CenterOfRotation=[2.0, 46.45000076293945, 0.0],
+    CameraPosition=[2.0, 46.45000076293945, 64.20057971900172],
+    CameraFocalPoint=[2.0, longitude, latitude],
     CameraFocalDisk=1.0,
-    CameraParallelScale=16.531446698647343,
+    CameraParallelScale=1.616332737900287,
     OSPRayMaterialLibrary=materialLibrary1,
 )
 
-SetActiveView(None)
+# Create the second 'Render View' (for the second window)
+renderView2 = CreateView('RenderView')
+renderView2.Set(
+    ViewSize=[961, 479],
+    InteractionMode='2D',
+    CenterOfRotation=[2.0, 46.45000076293945, 0.0],
+    CameraPosition=[2.0, 46.45000076293945, 64.20057971900172],
+    CameraFocalPoint=[2.0, 45.2815517, 5.8441357],
+    CameraFocalDisk=1.0,
+    CameraParallelScale=16.616332737900287, # zoom
+    OSPRayMaterialLibrary=materialLibrary1,
+)
 
 # ----------------------------------------------------------------
-# setup view layouts
+# Create two separate layouts, one for each render view
 # ----------------------------------------------------------------
 
-# create new layout object 'Layout #1'
+# Create first layout for the first window (layout1)
 layout1 = CreateLayout(name='Layout #1')
 layout1.AssignView(0, renderView1)
-layout1.SetSize(1418, 779)
+layout1.SetSize(961, 479)
+
+# Create second layout for the second window (layout2)
+layout2 = CreateLayout(name='Layout #2')
+layout2.AssignView(0, renderView2)
+layout2.SetSize(961, 479)
 
 # ----------------------------------------------------------------
-# restore active view
+# restore active view to the first render view
 SetActiveView(renderView1)
 # ----------------------------------------------------------------
 
@@ -133,7 +153,7 @@ if (ShowIsolines) :
 departements20140306100mshp = GDALVectorReader(registrationName='departements-20140306-100m.shp', FileName='/home/pierre/Documents/Info5/Visu/VISU_DONNEES_INFO5/Projet/departements-20140306-100m-shp/departements-20140306-100m.shp')
 
 # ----------------------------------------------------------------
-# setup the visualization in view 'renderView1'
+# setup the visualization in the first render view
 # ----------------------------------------------------------------
 
 # show data from departements20140306100mshp
@@ -239,6 +259,116 @@ tempratureSubdivisionDisplay.SetScalarBarVisibility(renderView1, True)
 if (ShowIsolines) :
 	# show color legend
 	temperatureIsolinesDisplay.SetScalarBarVisibility(renderView1, True)
+	
+	
+# ----------------------------------------------------------------
+# setup the visualization in the second render view
+# ----------------------------------------------------------------
+
+# show data from departements20140306100mshp
+departements20140306100mshpDisplay2 = Show(departements20140306100mshp, renderView2, 'GeometryRepresentation')
+
+# trace defaults for the display properties.
+departements20140306100mshpDisplay2.Set(
+    Representation='Surface',
+    AmbientColor=[0.0, 0.0, 0.0],
+    ColorArrayName=['POINTS', ''],
+    DiffuseColor=[0.0, 0.0, 0.0],
+    LineWidth=1.5,
+    Assembly='Hierarchy',
+)
+
+# show data from tempratureSubdivision
+tempratureSubdivisionDisplay2 = Show(tempratureSubdivision, renderView2, 'GeometryRepresentation')
+
+# get color transfer function/color map for 'Temperature'
+temperatureLUT2 = GetColorTransferFunction('Temperature')
+temperatureLUT2.Set(
+    RGBPoints=[
+        # scalar, red, green, blue
+        -16.690899077217523, 0.0564, 0.0564, 0.47,
+        -10.812571624600016, 0.243, 0.46035, 0.81,
+        -6.4653183994513554, 0.356814, 0.745025, 0.954368,
+        -1.8872033118429687, 0.6882, 0.93, 0.91791,
+        0.71268630027771, 0.899496, 0.944646, 0.768657,
+        3.4603028677646783, 0.957108, 0.833819, 0.508916,
+        7.499785953079805, 0.927521, 0.621439, 0.315357,
+        12.34719991304253, 0.8, 0.352, 0.16,
+        17.56668549908852, 0.59, 0.0767, 0.119475,
+    ],
+    NumberOfTableValues=12,
+    ScalarRangeInitialized=1.0,
+)
+
+# trace defaults for the display properties.
+tempratureSubdivisionDisplay2.Set(
+    Representation='Surface',
+    ColorArrayName=['POINTS', 'Temperature'],
+    LookupTable=temperatureLUT,
+)
+
+# init the 'Piecewise Function' selected for 'ScaleTransferFunction'
+tempratureSubdivisionDisplay2.ScaleTransferFunction.Points = [-16.690899077217523, 0.0, 0.5, 0.0, 17.2422944288289, 1.0, 0.5, 0.0]
+
+# init the 'Piecewise Function' selected for 'OpacityTransferFunction'
+tempratureSubdivisionDisplay2.OpacityTransferFunction.Points = [-16.690899077217523, 0.0, 0.5, 0.0, 17.2422944288289, 1.0, 0.5, 0.0]
+
+if (ShowIsolines) :
+	# show data from temperatureIsolines
+	temperatureIsolinesDisplay2 = Show(temperatureIsolines, renderView2, 'GeometryRepresentation')
+
+	# trace defaults for the display properties.
+	temperatureIsolinesDisplay2.Set(
+		Representation='Surface',
+		ColorArrayName=['POINTS', 'Temperature'],
+		LookupTable=temperatureLUT,
+		LineWidth=3.0,
+	)
+
+	# init the 'Piecewise Function' selected for 'ScaleTransferFunction'
+	temperatureIsolinesDisplay2.ScaleTransferFunction.Points = [0.27569767580568616, 0.0, 0.5, 0.0, 0.2757587134838104, 1.0, 0.5, 0.0]
+
+	# init the 'Piecewise Function' selected for 'OpacityTransferFunction'
+	temperatureIsolinesDisplay2.OpacityTransferFunction.Points = [0.27569767580568616, 0.0, 0.5, 0.0, 0.2757587134838104, 1.0, 0.5, 0.0]
+
+# show data from ventFleches
+ventFlechesDisplay2 = Show(ventFleches, renderView2, 'GeometryRepresentation')
+
+# trace defaults for the display properties.
+ventFlechesDisplay2.Set(
+    Representation='Surface',
+    AmbientColor=[0.7411764860153198, 0.7411764860153198, 0.7411764860153198],
+    ColorArrayName=[None, ''],
+    DiffuseColor=[0.7411764860153198, 0.7411764860153198, 0.7411764860153198],
+    LineWidth=2.0,
+)
+
+# init the 'Piecewise Function' selected for 'ScaleTransferFunction'
+ventFlechesDisplay2.ScaleTransferFunction.Points = [5.01715033100156, 0.0, 0.5, 0.0, 9.782604853913682, 1.0, 0.5, 0.0]
+
+# init the 'Piecewise Function' selected for 'OpacityTransferFunction'
+ventFlechesDisplay2.OpacityTransferFunction.Points = [5.01715033100156, 0.0, 0.5, 0.0, 9.782604853913682, 1.0, 0.5, 0.0]
+
+# setup the color legend parameters for each legend in this view
+
+# get color legend/bar for temperatureLUT in view renderView1
+temperatureLUTColorBar2 = GetScalarBar(temperatureLUT, renderView1)
+temperatureLUTColorBar2.Set(
+    WindowLocation='Upper Right Corner',
+    Title='Temperature',
+    ComponentTitle='',
+)
+
+# set color bar visibility
+temperatureLUTColorBar2.Visibility = 1
+
+# show color legend
+tempratureSubdivisionDisplay2.SetScalarBarVisibility(renderView1, True)
+
+if (ShowIsolines) :
+	# show color legend
+	temperatureIsolinesDisplay2.SetScalarBarVisibility(renderView1, True)
+
 
 # ----------------------------------------------------------------
 # setup color maps and opacity maps used in the visualization
@@ -292,13 +422,13 @@ SetActiveSource(a12dec6hnc)
 ## You may need to add some code at the end of this python script depending on your usage, eg:
 #
 ## Render all views to see them appears
-# RenderAllViews()
+RenderAllViews()
 #
 ## Interact with the view, usefull when running from pvpython
 Interact()
 #
 ## Save a screenshot of the active view
-# SaveScreenshot("path/to/screenshot.png")
+SaveScreenshot("testParaview.png")
 #
 ## Save a screenshot of a layout (multiple splitted view)
 # SaveScreenshot("path/to/screenshot.png", GetLayout())

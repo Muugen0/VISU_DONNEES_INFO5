e89b2bc23e028c0f4139fe415273415a3212b876
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2025-12-17 12:07:00 +0100

    avancée TP3

--- TP3/script.py
+++ TP3/script.py
@@ -1,11 +1,10 @@
-import math
 import matplotlib.pyplot as plt
 import random
 import numpy as np
 
 def generate_data(max_value, length):
-    return [((random.random()*2)-1)*max_value for _ in range(length)]
-    
+    return [round(((random.random()*2)-1)*max_value,2) for _ in range(length)]
+
 def generate_data_lisse(max_value, length):
     t = np.linspace(-np.pi, np.pi, length)
     raw = (
@@ -16,171 +15,175 @@ def generate_data_lisse(max_value, length):
     )
     raw_min, raw_max = np.min(raw), np.max(raw)
     norm = 2*(raw - raw_min)/(raw_max - raw_min) - 1
-    return norm * max_value
+    return [round(n,2) for n in (norm * max_value)]
 
 
-def uneDecomposition(data):
+def une_decomposition(data : list) :
     new_data = []
-    coeff = []
-    for i in range(0, len(data)-1, 2):
-        moyenne = (data[i]+data[i+1])/2
-        new_data.append(moyenne)
-        coeff.append(data[i]-moyenne)
-    return new_data,coeff
+    coefs = []
+    n = len(data)-1
+    for i in range(0,n,2) :
+        moy = (data[i] + data[i+1]) /2
+        coef = data[i] - moy
+        new_data.append(moy)
+        coefs.append(coef)
+    return new_data, coefs
 
-def uneRecomposition(data, coeff):
+def une_recomposition(data, coefs) :
     new_data = []
-    for i in range(len(data)):
-        new_data.append(data[i] + coeff[i])
-        new_data.append(data[i] - coeff[i])
+    for i in range(len(coefs)) :
+        new_data.append(data[i] + coefs[i])
+        new_data.append(data[i] - coefs[i])
     return new_data
 
-def Decomposition(data):
+def decomposition_totale(data) :
     new_data = data
-    coeff = []
-    while len(new_data) > 1:
-        new_data,new_coeff = uneDecomposition(new_data)
-        coeff.insert(0, new_coeff)
-    return new_data,coeff
+    coefs = []
+    while(len(new_data) > 1) :
+        new_data, coefstmp = une_decomposition(new_data)
+        coefs.extend(coefstmp)
+    return new_data, coefs
 
-def Recomposition(data, coeff):
+def recomposition_totale(data, coefs : list) :
+    new_coefs = coefs.copy()
     new_data = data
-    for current_coeff in coeff:
-        new_data = uneRecomposition(new_data, current_coeff)
-    return new_data
-
-def RemoveSmallCoeff(coeff, seuil):
-    return [c if abs(c) > seuil else 0.0 for c in coeff]
-
-def GlobalRemoveSmallCoeff(coeff, seuil):
-    return [RemoveSmallCoeff(c,seuil) for c in coeff]
-
-def RecompositionRemoveSmallCoeff(data, coeff, seuil):
-    new_coeff = GlobalRemoveSmallCoeff(coeff, seuil)
-    return Recomposition(data, new_coeff)
-
-def Error1(data, seuil):
-    decomposition,coeff = Decomposition(data)
-    new_data = RecompositionRemoveSmallCoeff(decomposition, coeff, seuil)
-    sum = 0
-    for i in range(len(data)):
-    	sum += abs(data[i] - new_data[i])
-    return sum/len(data)
     
-def Error2(data, seuil):
-    decomposition,coeff = Decomposition(data)
-    new_data = RecompositionRemoveSmallCoeff(decomposition, coeff, seuil)
+    while(len(new_coefs) > 1) :
+        boucle_coefs = []
+        for i in range(len(new_data)) :
+            boucle_coefs.insert(0,new_coefs.pop(-1))
+        new_data = une_recomposition(new_data, boucle_coefs)
+    return new_data
+
+def zero_coef_seuil(coefs, seuil):
+    return [c if c < seuil else 0.00 for c in coefs]
+
+def Calcul_erreur(data, new_data):
     sum = 0
     for i in range(len(data)):
-    	sum += (data[i] - new_data[i])**2
-    return (math.sqrt(sum))/len(data)
-
-def histo(data, seuil):
-    new_data,coeff = Decomposition(data)
-    abs_coeff = [abs(c) for current_coeff in coeff for c in current_coeff]
-    coeff_min = int(min(abs_coeff))
-    coeff_max = int(max(abs_coeff))
-    bins = range(coeff_min, coeff_max + 2)
+        sum += abs(data[i] - new_data[i])
+    return sum/len(data)
+            
+def Histograme_coefs(coefs, seuil) :
+    coefs_abs = [abs(c) for c in coefs]
+    data_range = range(int(min(coefs_abs)), int(max(coefs_abs)))
     plt.figure(figsize=(6,4))
-    plt.hist(abs_coeff, bins=bins, color='skyblue', edgecolor='black')
-    plt.title("Répartition des valeurs absolues des coefficients de détails")
-    plt.xlabel("coefficient de détail")
-    plt.ylabel("nombre d'occurences")
+    plt.subplot(2,1,1)
+    plt.hist(coefs_abs, bins=data_range, color='skyblue', edgecolor='black')
+    plt.title("Valeurs absolues des coefficients de détail")
+    plt.xlabel("Coefficients de détail")
+    plt.ylabel("Occurences")
     plt.axvline(x=seuil, linestyle='--', linewidth=2, color='red')
     plt.grid(True, linestyle='--', alpha=1)
     plt.show()
-
-def graphError(data):
-    new_data,coeff = Decomposition(data)
-    abs_coeff = [abs(c) for current_coeff in coeff for c in current_coeff]
-    seuil_max = max(abs_coeff)
-    step = seuil_max/100
-    res1 = []
-    res2 = []
-    for i in np.arange(0,seuil_max,step):
-    	res1.append([i,Error1(data,i)])
-    	res2.append([i,Error2(data,i)])
-    x = [pair[0] for pair in res1]
-    y1 = [pair[1] for pair in res1]
-    y2 = [pair[1] for pair in res2]
+    
+def Graphe_Erreur(data) :
+    new_data, coefs = decomposition_totale(data)
+    coefs_abs = [abs(c) for c in coefs]
+    coef_max = max(coefs_abs)
+    step = (coef_max / 100)
+    x = []
+    y = []
+    for i in np.arange(0,coef_max,step):
+        coefs_zeros = zero_coef_seuil(coefs,i)
+        new_data_zeros = recomposition_totale(new_data, coefs_zeros)
+        val = Calcul_erreur(new_data, new_data_zeros)
+        x.append(i)
+        y.append(val)
     plt.figure(figsize=(6,4))
-    plt.plot(x, y1, marker='o', linestyle='-', color='blue', label='valeur absolue')
-    plt.plot(x, y2, marker='o', linestyle='-', color='red', label='racine carré')
+    plt.subplot(2,1,2)
+    plt.plot(x, y, marker='o', linestyle='-', color='blue')
     plt.title("Erreur en fonction du seuil de mise à zéro des coefficients de détails")
     plt.xlabel("seuil")
     plt.ylabel("erreur")
-    plt.legend()
     plt.show()
-
-def behavior(max_value, length, seuil):
-    # données irrégulières
-    x = np.arange(length)
     
-    data1 = generate_data(max_value, length)
-    new_data1, coeff1 = Decomposition(data1)
-    new_data2 = RecompositionRemoveSmallCoeff(new_data1, coeff1, seuil)
-
+def graphe_comportement(data, max, taille, seuil) :
+    
+    # data irregulière
+    data_irreg = data
+    new_data, coefs = decomposition_totale(data_irreg)
+    coefs = zero_coef_seuil(coefs,seuil)
+    new_data_irreg = recomposition_totale(new_data,coefs)
+    
+    # data lisse
+    data_lisse = generate_data_lisse(max,taille)
+    new_data, coefs = decomposition_totale(data_lisse)
+    coefs = zero_coef_seuil(coefs,seuil)
+    new_data_lisse = recomposition_totale(new_data,coefs)
+    
+    x = np.arange(taille)
     plt.figure(figsize=(6,8))
     plt.subplot(2,1,1)
-    plt.plot(x, data1, label="données originales")
-    plt.plot(x, new_data2, label="données reconstruites")
+    plt.plot(x, data_irreg, label="données originales")
+    plt.plot(x, new_data_irreg, label="données reconstruites")
     plt.title("Comportement sur des données irrégulières")
     plt.legend()
     plt.grid(True)
-
-    # données lisses
-    data2 = generate_data_lisse(max_value, length)
-    new_data3, coeff2 = Decomposition(data2)
-    new_data4 = RecompositionRemoveSmallCoeff(new_data3, coeff2, seuil)
     
     plt.subplot(2,1,2)
-    plt.plot(x,data2, label="données originales")
-    plt.plot(x, new_data4, label="données reconstruites")
+    plt.plot(x,data_lisse, label="données originales")
+    plt.plot(x, new_data_lisse, label="données reconstruites")
     plt.title("Comportement sur des données lisses")
     plt.legend()
     plt.grid(True)
     plt.show()
 
-#-----------------------------------------------------------------
-max_value = int(input("Valeur max des données : "))
-length = int(input("Nombre de données : "))
-data = generate_data(max_value, length)
-seuil = float(input("Valeur seuil : "))
-print("Initial data : ")
-print(data)
-data_decomposition,coeff = uneDecomposition(data)
-print("One decomposition: ")
-print(data_decomposition)
-print("Coeff : ")
-print(coeff)
-data_recomposition = uneRecomposition(data_decomposition, coeff)
-print("One recomposition : ")
-print(data_recomposition)
-print("---------------------------------------------------------")
-print("Initial data : ")
-print(data)
-data_decomposition,coeff = Decomposition(data)
-print("Total decomposition : ")
-print(data_decomposition)
-print("Coeff : ")
-print(coeff)
-data_recomposition = Recomposition(data_decomposition, coeff)
-print("Total Recomposition : ")
-print(data_recomposition)
-print("---------------------------------------------------------")
-print("Initial data : ")
-print(data)
-data_decomposition,coeff = Decomposition(data)
-print("Total decomposition : ")
-print(data_decomposition)
-print("Coeff : ")
-print(coeff)
-data_recomposition = RecompositionRemoveSmallCoeff(data_decomposition, coeff, seuil)
-print("Total recomposition - remove small coeff : ")
-print(data_recomposition)
-print("---------------------------------------------------------")
-print("Error calcul with absolute value :", Error1(data, seuil))
-print("Error calcul with square root : ", Error2(data, seuil))
-histo(data, seuil)
-graphError(data)
-behavior(max_value, length, seuil)
+if __name__ == '__main__':
+    max_main = int(input("Quelle est la valeur maximale du signal ?\n"))
+    length_main = int(input("Combien de valeurs voulez-vous dans le signal ?\n"))
+    while (length_main % 2 != 0) :
+        print("le nombre de valeur dans le signal doit être un multipde de 2")
+        length_main = int(input("Combien de valeurs voulez-vous dans le signal ?\n"))
+    seuil_main = int(input("A partir de quel seuil voulez-vous supprimer les ceofficients ?\n"))
+    data_main = generate_data(max_main,length_main)
+    # data_main = [12, 10, 7, 5, 6, 10, 4, 8]
+    print(f"Données originales : {data_main}")
+    
+    print("=======================================================================================")
+    data_1decomp, coefs_1decomp = une_decomposition(data_main)
+    print(f"Données décomposés une fois : {data_1decomp}")
+    print(f"Coefficents d'une décomposition : {coefs_1decomp}")
+    data_1recomp = une_recomposition(data_1decomp, coefs_1decomp)
+    print(f"Données resomposés une fois : {data_1recomp}")
+    print(f"Comparaison arrondie à deux chiffres après la virgule :")
+    print("Original\t--\tRecomposée")
+    i = 0
+    while (i < len(data_main) and i < len(data_1recomp)) :
+        print(f"{round(data_main[i],2)}\t--\t{round(data_1recomp[i],2)}")
+        i += 1
+    
+    print("=======================================================================================")
+    data_decomp_tot, coefs_decomp_tot = decomposition_totale(data_main)
+    print(f"Données totalement décomposés : {data_decomp_tot}")
+    print(f"Coefficents de décomposition totale : {coefs_decomp_tot}")
+    data_recomp_tot = recomposition_totale(data_decomp_tot, coefs_decomp_tot)
+    print(f"Données totalement resomposée : {data_recomp_tot}")
+    print(f"Comparaison arrondie à deux chiffres après la virgule :")
+    print("Original\t--\tRecomposée")
+    i = 0
+    while (i < len(data_main) and i < len(data_recomp_tot)) :
+        print(f"{round(data_main[i],2)}\t--\t{round(data_recomp_tot[i],2)}")
+        i += 1
+    
+    print("=======================================================================================")
+    coefs_decomp_tot_zeros = zero_coef_seuil(coefs_decomp_tot, seuil_main)
+    data_recomp_tot_zeros = recomposition_totale(data_decomp_tot, coefs_decomp_tot_zeros)
+    print(f"Données totalement resomposée avec zeros au seuil: {coefs_decomp_tot_zeros}")
+    print(f"Comparaison arrondie à deux chiffres après la virgule :")
+    print("Original\t--\tRecomposée")
+    i = 0
+    while (i < len(data_main) and i < len(coefs_decomp_tot_zeros)) :
+        print(f"{round(data_main[i],2)}\t--\t{round(coefs_decomp_tot_zeros[i],2)}")
+        i += 1
+    
+    
+    print("=======================================================================================")
+    erreur_recomp_tot = Calcul_erreur(data_main, data_recomp_tot)
+    print(f"Erreur entre les données de départ et les données recomposées : {erreur_recomp_tot}")
+    erreur_recomp_tot_zeros = Calcul_erreur(data_main, data_recomp_tot_zeros)
+    print(f"Erreur entre les données de départ et les données recomposées après zéros : {erreur_recomp_tot}")
+    
+    Histograme_coefs(coefs_decomp_tot, seuil_main)
+    Graphe_Erreur(data_main)
+    graphe_comportement(data_main, max_main, length_main, seuil_main)
